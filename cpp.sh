b2e9191498c168a25f1c1e8c457a691da4f0e3a9
Daniel <131325098+dddanielliu@users.noreply.github.com>
2023-10-19 10:26:53 +0800

    Update cpp.sh

--- cpp.sh
+++ cpp.sh
@@ -1,3 +1,3 @@
 file="$1"
 # /usr/bin/g++ -std=gnu++17 -Wall -Wextra -Winit-self -Wold-style-cast -Woverloaded-virtual -Wuninitialized -Wmissing-declarations -Wshadow -O2 -pipe -static -s -pedantic -D_FORTIFY_SOURCE=2 -fstack-protector --param ssp-buffer-size=4 -fPIE -pie -Wl,-z,relro,-z,now ${file%.*}.cpp
-/usr/bin/g++ -std=gnu++17 -Wall -Wextra -Winit-self -Wold-style-cast -Woverloaded-virtual -Wuninitialized -Wmissing-declarations -Wshadow -O2 -pipe -static -s -pedantic -D_FORTIFY_SOURCE=2 -fstack-protector --param ssp-buffer-size=4 -fPIE -pie -Wl,-z,relro,-z,now "$file"
+/usr/bin/g++ -std=gnu++17 -Wall -Wextra -Winit-self -Wold-style-cast -Woverloaded-virtual -Wuninitialized -Wmissing-declarations -Wmisleading-indentation -Wshadow -O2 -pipe -static -s -pedantic -D_FORTIFY_SOURCE=2 -fstack-protector --param ssp-buffer-size=4 -fPIE -pie -Wl,-z,relro,-z,now "$file"
